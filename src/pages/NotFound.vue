746d7a34579cde9812736e1c93bd31583fa77cf6
Jhony Souza <jhony.jss@gmail.com>
2021-12-22 16:11:43 -0300

    refactor: creating pages and change the pages name

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="col-full">
+    <h1>NOT FOUND</h1>
+    <router-link :to="{ name: 'Home' }">Read some cool threads</router-link>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,44 @@
 import { createWebHistory, createRouter } from 'vue-router';
-import PageHome from '@/components/PageHome';
-import PageThreadShow from '@/components/PageThreadShow';
+import Home from '@/pages/Home';
+import ThreadShow from '@/pages/ThreadShow';
+import NotFound from '@/pages/NotFound';
+import sourceData from '@/data.json';
 
 const routes = [
   {
     name: 'Home',
     path: '/',
-    component: PageHome,
+    component: Home,
   },
 
   {
     name: 'ThreadShow',
-    path: '/thread/:id',
-    component: PageThreadShow,
+    path: '/thread/show/:id',
+    component: ThreadShow,
     props: true,
+    beforeEnter: (to, from, next) => {
+      // check if thread exists
+      const threadExists = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
+
+      if (threadExists) {
+        return next();
+      } else {
+        // preserve the query and hash ( URL )
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+    },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound,
   },
 ];
 
