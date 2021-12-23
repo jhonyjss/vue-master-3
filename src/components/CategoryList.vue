4ff7bc8f6397e4e0274033855e6e837e4d7e0517
Jhony Souza <jhony.jss@gmail.com>
2021-12-22 21:10:55 -0300

    feat: category page

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,35 @@
+<template>
+  <ForumList
+    v-for="category in categories"
+    :key="category.id"
+    :forums="getForumsForCategory(category)"
+    :category-name="category.name"
+  />
+</template>
+
+<script>
+import sourceData from '@/data.json';
+import ForumList from '@/components/ForumList';
+
+export default {
+  components: {
+    ForumList,
+  },
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  methods: {
+    getForumsForCategory(category) {
+      return sourceData.forums.filter(
+        (forum) => forum.categoryId === category.id
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
