d863ea59d8997ed8c0a1c261fd04b612113aa577
Jhony Souza <jhony.jss@gmail.com>
2022-01-04 10:46:33 -0300

    fix: showing loading spinner

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
   <the-navbar />
   <div class="container">
-    <router-view />
+    <router-view v-show="showPage" @ready="onPageReady" />
+    <AppSpinner v-show="!showPage" />
   </div>
 </template>
 
 <script>
 import TheNavbar from '@/components/TheNavbar'
 import { mapActions } from 'vuex'
-
+import NProgress from 'nprogress'
 export default {
   name: 'App',
   components: { TheNavbar },
+  data() {
+    return {
+      showPage: false,
+    }
+  },
   methods: {
     ...mapActions(['fetchAuthUser']),
+    onPageReady() {
+      this.showPage = true
+      NProgress.done()
+    },
   },
-
   created() {
     this.fetchAuthUser()
+    NProgress.configure({
+      speed: 200,
+      showSpinner: false,
+    })
+    this.$router.beforeEach(() => {
+      this.showPage = false
+      NProgress.start()
+    })
   },
 }
 </script>
 
 <style>
 @import 'assets/style.css';
+@import '~nprogress/nprogress.css';
+#nprogress .bar {
+  background: #57ad8d !important;
+}
 </style>
